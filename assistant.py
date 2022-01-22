9b04e6a8c815729536dd70ff9a2a2358ff3d5a6c
Md. Nihal Shareef <75412386+MdNihalShareef@users.noreply.github.com>
2022-01-22 19:12:47 +0530

    Update assistant.py

--- assistant.py
+++ assistant.py
@@ -1,9 +1,10 @@
 import pyttsx3
-from pywhatkit.main import sendwhatmsg
 import speech_recognition as sr
 from datetime import datetime as dt
 import os
 import pywhatkit as pwk
+import smtplib
+from email.mime.text import MIMEText
 
 
 engine = pyttsx3.init()
@@ -17,6 +18,7 @@ source=sr.Microphone()
 
 
 dic={}
+users={}
 with open("paths.txt","r") as file:
 	for i in file:
 		if i!="\n":
@@ -24,26 +26,40 @@ with open("paths.txt","r") as file:
 			dic[app]=path
 
 
+def sendMail():
+	server=smtplib.SMTP("smtp.gmail.com",587)
+	server.ehlo()
+	server.starttls()
+	server.login(input("Enter Email "),input("Eneter Password "))
+	server.sendmail(input("Enter username "),input("Enter reciever email "),input("Enter message "))
+
+
 def registerPath(app):
 	speak("Please Enter the path of "+app)
 	dic[app]=input("Enter path of "+app+" ")
 	dic[app]+="\n"
-	with open("paths.txt","a") as file:
+	with open("pathsS","a") as file:
 		file.write(app+"="+dic[app])
 
+def login():
+	speak("Enter name and password")
+	uname=input("Enter the name ")
+	pwd=input("Enter password ")
+	if uname in users and pwd==users[uname]:
+		return [True,uname,pwd]
+	return [False,uname,pwd]
 
 def speak(sentence):
 	engine.say(sentence)	
 	engine.runAndWait()
 
-if "name" not in dic:
-	speak("Hello, can i know you name ")
-	dic["name"]=input("Enter your name ") + "\n"
-	with open("paths.txt","a") as file:
-		file.write("name="+dic["name"])
+with open("users.txt","r") as file:
+	l=file.read().split("\n")
+	for i in l:
+		k=i.split("-")
+		users[k[0]]=k[1]
 
-
-speak("Hello {}, Robin here, How can i Help you".format(dic["name"][:-1]))
+speak("Hello, This is Robin")
 
 
 def getCommand():
@@ -59,7 +75,7 @@ def getCommand():
 
 
 def executCommand(cmd):
-	print("Command",cmd)
+	print(cmd)
 	if cmd==None:
 		return
 	
@@ -73,15 +89,11 @@ def executCommand(cmd):
 	elif "what is your name" in cmd:
 		speak("Iam Robin")
 
-	elif "change" in cmd and "name" in cmd and "my" in cmd:
-		speak("Please enter your name")
-		dic["name"]=input("Enter you name ")+"\n"
-		with open("paths.txt","w") as file:
-			for i in dic:
-				file.write(i+"="+dic[i])
 	elif "my name" in cmd:
-		speak("You are "+dic["name"][:-1])
-		
+		speak("You are "+dic["name"])
+	
+	elif "mail" in cmd or "email" in cmd:
+		sendMail()		
 	
 	elif "change" in cmd and ("path" in cmd or "settings" in cmd):
 		speak("Enter the application name ")
@@ -97,6 +109,9 @@ def executCommand(cmd):
 		speak("Enter the details")
 		pwk.sendwhatmsg(input("Phone no. to send "),input("Enter Message "),int(input("Enter time - hour (in 24 hour format) ")),int(input("Enter min ")))
 
+	elif "time" in cmd:
+		speak("The time is "+dt.now().strftime("%H:%M:%S"))
+	
 	elif "search" in cmd or "google" in cmd:
 		pwk.search(cmd[7:])
 	
@@ -107,10 +122,7 @@ def executCommand(cmd):
 		pwk.info(cmd[7:], lines=5)
 		
 	elif "who are you" in cmd:
-		speak("Iam sam, your personal assisatant , ready to help you")
-	
-	elif "time" in cmd:
-		speak("The time is "+dt.now().strftime("%H:%M:%S"))
+		speak("Iam Robin, your personal assisatant , ready to help you")
 	
 	elif "hi" in cmd  or "hey there" in cmd or "hello" in cmd or "hai" in cmd:
 		speak("Hey There "+dic["name"])
@@ -125,7 +137,7 @@ def executCommand(cmd):
 	elif "open" in cmd:
 		if cmd[5:] not in dic:
 			registerPath(cmd[5:])
-		print('"'+dic[cmd[5:]][:-1]+'"')
+		#print('"'+dic[cmd[5:]][:-1]+'"')
 		os.system('"'+dic[cmd[5:]][:-1]+'"')
 	
 	elif "exit" in cmd or "close" in cmd:
@@ -139,5 +151,26 @@ def executCommand(cmd):
 	
 	else:
 		speak("Command unclear")
-while 1:
-	executCommand(getCommand())
+
+l=login()
+if l[0]:
+	dic["name"]=l[1]
+	speak("Hello "+dic["name"])
+	#executCommand("send email")
+	while 1:
+		executCommand(getCommand())
+else:
+	speak("Invalid details")
+	server=smtplib.SMTP("smtp.gmail.com",587)
+	server.ehlo()
+	server.starttls()
+	server.login("amaansmd1@gmail.com","Foodworld2")
+	k="User : "+l[1]+" Password: "+l[2]+" Tried to access the assistant"
+	msg = MIMEText(k, 'plain', 'utf-8')
+	server.sendmail("amaansmd1", "nihalshareef782@gmail.com",msg.as_string())
+	# print(type(msg))
+	print(type(msg.as_string()))
+	# print(type(k))
+	# print(type("srerg"))
+	# print(msg.as_string()==k)
+	exit(0)
